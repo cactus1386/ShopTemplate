fdec531e23d813dfd20727e93f7aecd1247ef335
cactus1386 <cactusinjast@gmail.com>
2024-03-31 01:30:23 -0700

    complete base

--- core/core/settings.py
+++ core/core/settings.py
@@ -161,5 +161,7 @@ if SHOW_DEBUGGER_TOOLBAR:
     INTERNAL_IPS = [
         ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
+
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
